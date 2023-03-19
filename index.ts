56ae1820fcd87166a98e55cc472a75b10cb69d0e
hamza7saeed <126345476+hamza7saeed@users.noreply.github.com>
2023-03-19 15:01:55 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,31 @@
+/*Assignment No. 1: 
+Create an index.html file and use JavaScript in it to output "Hello World" using either console or document.write.
+Create a script.js file and link it with HTML and print the same output as the above assignment.
+
+console.log("Hello World, My name is Hamza, Do you know me?")// with the help of console.log, it is not helps us to refresh our html page on web, but otherwise, we use it to run our program on terminal.
+document.write("Hello World, My name is Hamza, Do you know me?");// this document.write help us to refresh our htlm page. */
+
+
+/*Assignment No. 2: 
+Write about the difference between errors and bugs and describe the types of errors and bugs. Define them.
+
+Errors: when we are writing our program or code in any programming language, while when we make any mistake compiler through an error.
+Bugs: A bugs occour after compiling the program as well as it is unexpexted flaws which is held while writing program we never see but after compile we see it. */
+
+/*Assignment No. 3: 
+Explain what statements and expressions are in JavaScript and highlight the difference between them.
+
+Expression: In js, it is set of values and functions or sign, when we join them both which lead to show us a result with the help of compiler. For example: y = 10,
+Statement: statements in javascript, is a set of instration which tells browser to what action it should perform.
+Difference: 1) Expression in Js works dependly, in contrast Statement works independly.
+2) Statement: It is assign a value to object, whereas Experation: it is assign to variable or array.*/
+
+/*Assignment No. 4: 
+Define syntax in JavaScript or any other programming language, and also explain how to use comments in JavaScript and the different types of comments available.
+
+Syntax: it is rule in programming language which is set of combination of symbols, experassion and statements who order to computer to perform according to it.
+Comments: bascily, comments function helps us to disable the command or code and it is not read by compiler.
+/* */: this Comment use by programmer if he want to disable the multiline functions.
+//: It is use when we have single line of code or programm and we donot want to compile it. */
+
+ 
